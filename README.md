010af50f9397b469f218a1cd6bede74860b01211
shmeowlex <shmeowlex@gmail.com>
2024-11-30 12:24:06 -0800

    initial

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # Codegrade-Submission-Renamer
 Used to rename CodeGrade submissions to student names for upload to MOSS
+
+## Quick Start
+1. Export submissions (all) from CodeGrade: https://help.codegrade.com/faq/exporting-submissions
+2. Place `rename.sh` into the directory downloaded from CodeGrade.
+3. Open terminal at directory.
+4. Run `chmod +x rename.sh`
+5. Run `./rename.sh NAME` where `NAME` is your first name.
+6. After done running a directory called `/processed` will be added to the directory downloaded from CodeGrade. Upload the submissions from this `/processed` directory to the proper MOSS directory.
---
+++ rename.sh
@@ -0,0 +1,28 @@
+# argument validation check
+if [ "$#" -ne 3 ]; then
+  echo "Usage: $0 <yourFirstName>"
+  exit 1
+fi
+
+# make directory for files
+mkdir ./processed
+
+# process
+for i in *; do
+  # check if directory exists
+  if [[ -d $i ]]; then 
+    # exclude processed directory
+    if [[ $i != "processed" ]] ; then
+      echo  "processed" $i
+      # get the filename to process
+      name=$i
+      #echo "Name is: " $name
+      nameNoSpace="$(echo -e "${name}" | tr -d '[:space:]')"
+      #remove the spaces from the directory name
+      ofname=${nameNoSpace:0:24}
+      ofname2=${nameNoSpace:7:24}
+      # rename the file into the new directory
+      cp ./"$name"/main.cpp  ./processed/"["$1"]"$ofname2-main.cpp
+    fi
+  fi
+done
